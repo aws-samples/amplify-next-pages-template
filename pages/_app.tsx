2bbcf4cac99a0bd308f69ac195525139b618b97b
Jay Raval <jay.ambrish.raval@gmail.com>
2024-04-16 13:47:51 -0700

    🚀

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
-import "@/styles/globals.css";
+import "@/styles/app.css";
 import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
+import config from "@/amplifyconfiguration.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(config);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
