de67245d0612d508da41251ff1cd497a525a8708
Jay Raval <jay.ambrish.raval@gmail.com>
2024-04-30 14:14:17 -0400

    update amplify config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "@/styles/app.css";
 import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
-import config from "@/amplifyconfiguration.json";
+import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-Amplify.configure(config);
+Amplify.configure(outputs);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
