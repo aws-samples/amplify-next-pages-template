0652aea98789b828f40e9744610528017ed6d65b
Jay Raval <jay.ambrish.raval@gmail.com>
2024-04-30 15:48:29 -0400

    clean install and update schema type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
-import { data, type Schema } from "@/amplify/data/resource";
+import type { Schema } from "@/amplify/data/resource";
 
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Schema["Todo"][]>([]);
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
